c3f142fddff51b41e42cfc7fb10295b1990cca65
Vedant Chainani <vedantchainani1084@gmail.com>
2025-01-10 21:12:11 +0530

    chore: remove unused fields

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  clearScreen: false,
   envPrefix: ['VITE_'],
   plugins: [TanStackRouterVite({}), react(), tailwindcss()],
   server: { port: 3000 },
